b2be2941be59399ee5c56350a36d9f377f4281a0
Foo Fangwei Kenneth <kfoofw@gmail.com>
2020-01-16 22:38:35 -0800

    Revert "created data dld script. stored data to subfolder."
    This reverts commit 59486bfb9bcef3ef987ed4bd556d35724a511b38.

--- src/data-download.py
+++
@@ -1,6 +0,0 @@
-import requests, zipfile, io
-r = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip")
-z = zipfile.ZipFile(io.BytesIO(r.content))
-
-# Assume run cmd line from project repo root. 
-z.extractall("./data/")
