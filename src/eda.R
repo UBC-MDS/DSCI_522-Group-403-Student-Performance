056b13e8735d4a41bef4116e8f5744c6ffc8c577
brayden tang <brayden.tang1@gmail.com>
2020-01-24 01:26:46 -0800

    fix a bug regarding a change in path.

--- src/eda.R
+++ src/eda.R
@@ -2,7 +2,8 @@
 # Date: January 24, 2020
 
 "This script creates box plots of all of the predictors, a correlation matrix,
-and a plot of the estimated distribution of G3. 
+and a plot of the estimated distribution of G3. This script assumes that it will
+be run in the root directory of the repository.
 
 Usage: eda.R <path_data> <directory_out>
 
@@ -15,9 +16,11 @@ library(tidyverse)
 library(ggcorrplot)
 library(docopt)
 
-main <- function(path_data, path_out) {
+main <- function(path_data, directory_out) {
   
 train_data <- read_csv(path_data)
+
+# Correlation Matrix
 
 correlation_mat <- ggcorrplot::cor_pmat(train_data %>%
                                           select_if(is.numeric))
@@ -26,7 +29,7 @@ ggsave(
     correlation_mat, 
     show.diag = TRUE, 
     title = "Correlation Plot of All Features (Pearon's R)"), 
-  filename = paste(path_out, "/correlation_matrix.png", sep = ""))
+  filename = paste(directory_out, "/correlation_matrix.png", sep = ""))
 
 plotting_data <- train_data %>%
   gather(key = predictor, value = value, -G3) %>%
@@ -39,7 +42,7 @@ ggsave(ggplot(data = plotting_data, aes(x = value, y = G3)) +
          theme_minimal() +
          theme(axis.title.y = element_blank()) +
          geom_jitter(alpha = 0.1),
-       filename = paste(path_out, "/box-plots.png", sep = ""),
+       filename = paste(directory_out, "/box-plots.png", sep = ""),
        width = 9, height = 12)
 
 # Absences
@@ -48,7 +51,7 @@ ggsave(ggplot(data = train_data, aes(x = absences, y = G3)) +
   geom_point() +
   theme_minimal() + 
   coord_flip(), 
-  filename = paste(path_out, "/absences.png", sep = ""))
+  filename = paste(directory_out, "/absences.png", sep = ""))
 
 # G3 Distribution
 
@@ -56,6 +59,10 @@ ggsave(ggplot(data = train_data, aes(x = G3, y = ..density..)) +
   geom_histogram(binwidth = 1) +
   geom_density() +
   theme_minimal(),
-  filename = paste(path_out, "/g3_hist.png", sep= ""))
+  filename = paste(directory_out, "/g3_hist.png", sep= ""))
 
 }
+
+opt <- docopt::docopt(doc)
+
+main(opt$path_data, opt$directory_out)
