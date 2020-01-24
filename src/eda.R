0eab56c95247cba65d2bad7b4eca3ae868d66973
brayden tang <brayden.tang1@gmail.com>
2020-01-24 11:44:26 -0800

    Finalize the EDA script and plots

--- src/eda.R
+++ src/eda.R
@@ -14,6 +14,8 @@ Options:
 
 library(tidyverse)
 library(ggcorrplot)
+library(ggridges)
+library(hrbrthemes)
 library(docopt)
 
 main <- function(path_data, directory_out) {
@@ -22,8 +24,8 @@ train_data <- read_csv(path_data)
 
 # Correlation Matrix
 
-correlation_mat <- ggcorrplot::cor_pmat(train_data %>%
-                                          select_if(is.numeric))
+correlation_mat <- cor(train_data %>% select_if(is.numeric))
+
 ggsave(
   ggcorrplot::ggcorrplot(
     correlation_mat, 
@@ -33,32 +35,46 @@ ggsave(
 
 plotting_data <- train_data %>%
   gather(key = predictor, value = value, -G3) %>%
-  filter(predictor != "absences")
+  filter(!predictor %in% c("G1", "G2", "absences"))
 
 ggsave(ggplot(data = plotting_data, aes(x = value, y = G3)) +
          facet_wrap(. ~ predictor, scale = "free", ncol = 3) +
          geom_boxplot() +
          coord_flip() +
          theme_minimal() +
-         theme(axis.title.y = element_blank()) +
-         geom_jitter(alpha = 0.1),
+         theme(axis.title.y = element_blank()),
        filename = paste(directory_out, "/box-plots.png", sep = ""),
        width = 9, height = 12)
 
 # Absences
 
-ggsave(ggplot(data = train_data, aes(x = absences, y = G3)) +
-  geom_point() +
-  theme_minimal() + 
-  coord_flip(), 
-  filename = paste(directory_out, "/absences.png", sep = ""))
+ggsave(train_data %>%
+  filter(G3 != 0) %>%
+  mutate(G3 = as.factor(G3)) %>% 
+  ggplot(., aes(x = absences, y = G3, group = G3)) +
+  geom_density_ridges(fill = "mediumseagreen") +
+  theme_ridges() + 
+  theme(legend.position = "none"), 
+filename = paste(directory_out, "/absences.png", sep = ""))
 
 # G3 Distribution
 
 ggsave(ggplot(data = train_data, aes(x = G3, y = ..density..)) +
   geom_histogram(binwidth = 1) +
   geom_density() +
-  theme_minimal(),
+  theme_minimal() +
+  annotate("label", x = 3, y = 0.13, label = paste(
+    "Mean:",
+    round(mean(train_data$G3), 2), 
+    "\n",
+    "Median:",
+    round(median(train_data$G3), 2),
+    "\n",
+    "Standard Deviation:",
+    round(sd(train_data$G3), 2))) +
+    geom_vline(aes(xintercept = quantile(train_data$G3, 0.1), color = "10th")) +
+    geom_vline(aes(xintercept = quantile(train_data$G3, 0.9), color = "90th")) + 
+    labs(color = "Percentile"),
   filename = paste(directory_out, "/g3_hist.png", sep= ""))
 
 }
